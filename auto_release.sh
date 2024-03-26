5be339ac8a6303a38e0ed4f2182f168f27b32263
Balázs Varga <warnyul@gmail.com>
2024-03-26 11:30:00 +0100

    feat: Setup auto release (#4)

---
+++ auto_release.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+function trim() {
+    while read -r line; do
+        echo "$line"
+    done
+}
+
+function readBuildToolsVersions() {
+    sdkmanager --list | grep build-tools | cut -d'|' -f1 | cut -d';' -f2 | trim
+}
+
+function autoRelease() {
+    if [ ! -f METADATA ]; then
+        wget https://android.googlesource.com/platform/prebuilts/cmdline-tools/+/refs/heads/main/METADATA
+    fi
+    local -r downloadUrl=$(grep -Eo 'https?://dl\.google\.com/android/repository/commandlinetools-linux-([[:digit:]]+)_latest\.zip' METADATA)
+    local -r commandLineToolsRevisionNumber=$(echo "$downloadUrl" | cut -d'_' -f1 | cut -d'-' -f3)
+    local -r buildToolsVersions=$(readBuildToolsVersions | sort | uniq )
+    local -r latest=$(echo "$buildToolsVersions" | grep -v '-' | tail -n 1)
+
+    while read -r line; do      
+        local dockerImageVersion="$line"-bionic-openjdk17
+        if [ "$(grep "\b${dockerImageVersion/-/\-}\b" .publishedVersions)" != "" ]; then
+            echo "skip ${dockerImageVersion}"
+        else
+            echo "build ${dockerImageVersion}"
+            if [ "$line" == "$latest" ]; then
+                local latestArg='--latest'
+            else
+                local latestArg=''
+            fi
+            ./build.sh --push --build-tools-version=${line} ${latestArg}
+            if [ $? == 0 ]; then
+                echo "$dockerImageVersion" >> .publishedVersions
+            fi
+        fi
+    done <<< "$buildToolsVersions"
+    rm -rf METADATA
+}
+
+autoRelease
--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ read -r -d '' USAGE <<- EOM
     --build-tools-version=VERSION \t Android build tools version. Default: 29.0.3\n
     --platform-version \t\t Android platform version. Default same as the build tools version.\n
     -l, --latest \t\t\t Flag build to latest\n
-    -d, --dry-run \t\t Push to a local repository. (always flag build to latest). Without push the image to the docker hub.\n
+    -d, --dry-run \t\t Push to a local repository. (always flag build to latest). Without push the image to the docker hub.\
+    -p, --push \t\t Push repository otherwise skips
     -h, --help \t\t\t Print usage description\n
 EOM
 
@@ -18,7 +19,8 @@ IMAGE_NAME=android-build-tools
 IMAGE=warnyul/"$IMAGE_NAME"
 
 # Parameters
-LOCAL_PUSH=false
+PUSH_TO_LOCAL=false
+PUSH=false
 LATEST=false
 BUILD_TOOLS_VERSION="30.0.0"
 PLATFORM_VERSION=$(echo "$BUILD_TOOLS_VERSION" | cut -d'.' -f1)
@@ -46,8 +48,11 @@ while [ $# -gt 0 ]; do
         ;;
         -d|--dry-run)
             LATEST=true
-            LOCAL_PUSH=true
+            PUSH_TO_LOCAL=true
             IMAGE=localhost:5000/"$IMAGE_NAME"
+        ;;
+        -p|--push)
+            PUSH=true
         ;;
         -h|--help|*)
             echo -e "\n Unknown argument: '$1'.\n See './build.sh --help'.\n"
@@ -67,15 +72,17 @@ docker build \
     -t "$IMAGE":"$IMAGE_VERSION" .
 
 # Start a local registry if necessary
-if "$LOCAL_PUSH" == "true"; then
+if [ "$PUSH_TO_LOCAL" == "true" && "$PUSH" == "true" ]; then
     docker run -d -p 5000:5000 --restart=always --name registry registry 2>&1
 fi 
 
 # Tag as latest if necessary
-if "$LATEST" == "true"; then
+if [ "$LATEST" == "true" ]; then
     docker tag "$IMAGE":"$IMAGE_VERSION" "$IMAGE":latest
 fi
 
 # Push the image
 echo push "$IMAGE":"$IMAGE_VERSION"
-docker push "$IMAGE"
+if [ "$PUSH" == "true" ]; then
+    docker push "$IMAGE"
+fi
